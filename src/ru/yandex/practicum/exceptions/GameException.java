fe9f595f149155eb7576277903ba19f6b6489672
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-05-19 03:30:05 +0300

    fix: добавлена пустая строка между конструкторами в GameException

--- src/ru/yandex/practicum/exceptions/GameException.java
+++ src/ru/yandex/practicum/exceptions/GameException.java
@@ -4,6 +4,7 @@ public class GameException extends RuntimeException {
     public GameException(String message) {
         super(message);
     }
+
     public GameException(String message, Throwable cause) {
         super(message, cause);
     }
